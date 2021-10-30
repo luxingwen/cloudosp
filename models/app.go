f7fd5313dbf152e10c5c3b24c8e1d931048d6c3e
Lewis Xingwen <shuaicode@gmail.com>
2021-10-30 22:34:29 +0800

    first commit

---
+++ models/app.go
@@ -0,0 +1,11 @@
+package models
+
+type App struct {
+	Id      int64
+	AppId   string
+	Name    string
+	ApiKey  string
+	SecKey  string
+	Creater string
+	Onwer   string
+}
---
+++ models/user.go
@@ -0,0 +1,9 @@
+package models
+
+type User struct {
+	Id       string
+	Uid      string
+	Username string
+	Passwd   string
+	Email    string
+}
